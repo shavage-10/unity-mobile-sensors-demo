251557a8108da48956b8a657573b18272847f5f6
malteerasmussen <malteerasmussen@gmail.com>
2022-03-12 15:15:10 +0100

    Try new input system

--- Mobile Sensor Demo/Assets/Orientation/Orientation.cs
+++ Mobile Sensor Demo/Assets/Orientation/Orientation.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Orientation : MonoBehaviour
 {
+    private void Start() {
+        // InputSystem.EnableDevice(UnityEngine.InputSystem.Accelerometer.current);
+        InputSystem.EnableDevice(UnityEngine.InputSystem.Accelerometer.current);
+    }
+
     private void Update() {
-        Debug.Log(Input.deviceOrientation);
+        Debug.Log(UnityEngine.InputSystem.Accelerometer.current);
+        // Debug.Log(Input.deviceOrientation);
+        // Debug.Log(UnityEngine.InputSystem.LinearAccelerationSensor.current.enabled);
     }
 }
